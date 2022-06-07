c8f2a490f702d4cd37e1841d19155f2d15ce055c
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-06-07 13:33:06 +0200

    create unique tournament details

---
+++ modules/Tournament/UniqueTournamentDetails.tsx
@@ -0,0 +1,87 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import useSWR from 'swr';
+import Meta from '../../components/Meta';
+import { SportContext } from '../../context/sportContext';
+import { FullEvent } from '../../model/Event';
+import { FullUniqueTournament, UniqueTournament } from '../../model/Tournament';
+import { CategoryLoading } from '../../styles/StyledCategories';
+import { API_BASENAME } from '../../util/fetch';
+import EventGroups from '../Event/EventGroups';
+
+const StyledUniqueTournamentDetails = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.bg1};
+  padding: 2rem;
+  border-radius: 10px;
+
+  & > h1 {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const StyledEvents = styled.section`
+  section {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width: 1000px;
+    margin-top: 2rem;
+
+    & > * {
+      flex-grow: 1;
+      flex-basis: 200px;
+      margin: 0.5rem;
+    }
+  }
+`;
+
+interface UniqueTournamentDetailsProps {
+  uniqueTournament: FullUniqueTournament;
+}
+
+export default function UniqueTournamentDetails({ uniqueTournament }: UniqueTournamentDetailsProps) {
+  const sportsContext = useContext(SportContext);
+  const imageUrl = `${API_BASENAME}/unique-tournament/${uniqueTournament.id}/image`;
+  const eventsUrl = `${API_BASENAME}/category/${uniqueTournament.category.id}/scheduled-events/${sportsContext?.date}`;
+  const { data, error } = useSWR(eventsUrl);
+
+  if (!data && !error) {
+    return <CategoryLoading />;
+  }
+  if (!data) {
+    return <div>An error has occurred...</div>;
+  }
+
+  const events: FullEvent[] = data.events.filter((event: FullEvent) => {
+    return event.tournament.uniqueTournament?.id === uniqueTournament.id;
+  });
+
+  const startDateTimestamp = new Date(uniqueTournament.startDateTimestamp * 1000).toLocaleDateString('en', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  });
+  const endDateTimestamp = new Date(uniqueTournament.endDateTimestamp * 1000).toLocaleDateString('en', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  });
+
+  return (
+    <StyledUniqueTournamentDetails>
+      <Meta title={uniqueTournament.name} description={`${uniqueTournament.name} - all events`} />
+      <img src={imageUrl} alt={uniqueTournament.name} />
+      <h1>{uniqueTournament.name}</h1>
+      <p>
+        {startDateTimestamp} - {endDateTimestamp}
+      </p>
+      {uniqueTournament.titleHolder && <small>Title holder: {uniqueTournament.titleHolder.name}</small>}
+      <StyledEvents>
+        <EventGroups events={events} />
+      </StyledEvents>
+    </StyledUniqueTournamentDetails>
+  );
+}
--- pages/tournament/[slug]/[id].tsx
+++ pages/tournament/[slug]/[id].tsx
@@ -1,20 +1,20 @@
 import { GetServerSideProps } from 'next';
 import React from 'react';
 import { API_BASENAME } from '../../../util/fetch';
-import { BasicEvent } from '../../../model/Event';
 import fetcher from '../../../util/fetch';
-import { UniqueTournament } from '../../../model/Tournament';
+import { FullUniqueTournament, UniqueTournament } from '../../../model/Tournament';
+import UniqueTournamentDetails from '../../../modules/Tournament/UniqueTournamentDetails';
 
 interface TournamentPageInterface {
-  tournament: UniqueTournament;
+  uniqueTournament: FullUniqueTournament;
 }
 
-export default function TournamentDetailsPage({ tournament }: TournamentPageInterface) {
-  return <div>{tournament.name}</div>;
+export default function TournamentDetailsPage({ uniqueTournament }: TournamentPageInterface) {
+  return <UniqueTournamentDetails key={uniqueTournament.id} uniqueTournament={uniqueTournament} />;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { params, res } = context;
+  const { params, res, req } = context;
 
   try {
     //@ts-ignore
@@ -22,7 +22,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
     const data = await fetcher(`${API_BASENAME}/unique-tournament/${id}`);
 
-    const props: TournamentPageInterface = { tournament: data.uniqueTournament };
+    if (data.error !== undefined) {
+      if (data.error.code === 404) {
+        return { notFound: true };
+      }
+    }
+
+    const props: TournamentPageInterface = { uniqueTournament: data.uniqueTournament };
 
     return { props: props };
   } catch (error) {
