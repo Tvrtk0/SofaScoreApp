134eea4a1fd403882adac419870bc03fc4c0bb6d
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-06-07 13:35:32 +0200

    update api basename

--- util/fetch.ts
+++ util/fetch.ts
@@ -3,7 +3,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default fetcher;
 
-export const API_BASENAME = 'https://api.sofascore.com/api/v1';
+export const API_BASENAME = 'https://api.sofascore1.com/api/v1';
 // https://api.sofascore1.com/api/v1
 // https://api.sofascore.com/api/v1
 // https://academy.dev.sofascore.com/api/v1
