fcfcb558db97cafc6e1faa4e0868f2f6c9ee650e
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-06-13 22:37:41 +0200

    add groupByUniqueTournament & groupBySport

--- util/group.ts
+++ util/group.ts
@@ -18,3 +18,29 @@ export const groupEventsByTournament = (data: FullEvent[]): FullEvent[][] => {
   });
   return groupedEvents;
 };
+
+export const groupEventsByUniqueTournament = (data: FullEvent[]): FullEvent[][] => {
+  const tournaments = data
+    .map((event) => event.tournament.uniqueTournament)
+    .filter((value, index, self) => index === self.findIndex((t) => t?.id === value?.id));
+
+  const groupedEvents: FullEvent[][] = [];
+  tournaments.forEach((uniqueTournament) => {
+    groupedEvents.push(
+      data.filter((event) => {
+        return event.tournament.uniqueTournament?.id === uniqueTournament?.id;
+      })
+    );
+  });
+  return groupedEvents;
+};
+
+export const groupEventsBySport = (data: FullEvent[]): FullEvent[][] => {
+  return data.reduce((result, currentValue) => {
+    (result[currentValue.tournament.category.sport.id] = result[currentValue.tournament.category.sport.id] || []).push(
+      currentValue
+    );
+
+    return result;
+  }, [] as FullEvent[][]);
+};
