55044f4dbf1944595864988f9e9ed209d84f5bfe
Tvrtk0 <tvrtkobabic2@gmail.com>
2022-06-13 22:39:15 +0200

    update layout & group by uniqueTournament & sport

--- modules/Category/CategoryDetails.tsx
+++ modules/Category/CategoryDetails.tsx
@@ -7,19 +7,49 @@ import { SportContext } from '../../context/sportContext';
 import { FullEvent } from '../../model/Event';
 import { CategoryLoading } from '../../styles/StyledCategories';
 import { API_BASENAME } from '../../util/fetch';
+import { groupEventsByUniqueTournament } from '../../util/group';
 import EventGroups from '../Event/EventGroups';
+import UniqueTournamentLink from '../Link/UniqueTournamentLink';
 
 const StyledCategoryDetails = styled.section`
   padding: 2rem;
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.bg1};
+  max-width: 1000px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileBreakpoint}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.breakpoint2}) {
     padding: 1rem;
   }
 
   & > h1 {
     text-align: center;
+  }
+
+  & > section {
+    & h3 {
+      text-align: center;
+      & a {
+        text-decoration: none;
+        color: ${({ theme }) => theme.colors.text};
+        &:hover {
+          color: ${({ theme }) => theme.colors.primary};
+        }
+      }
+    }
+
+    & > div {
+      margin-top: 2rem;
+      & > section {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 0 2rem;
+        & > * {
+          flex-grow: 1;
+          flex-basis: 0;
+        }
+      }
+    }
   }
 `;
 
@@ -45,21 +75,31 @@ export default function CategoryDetails({ id }: CategoryDetailsProps) {
   }
 
   const datePickerDate = new Date(sportsContext?.date!).toDateString();
-  const events: FullEvent[] = data.events.filter(
-    (event: FullEvent) => new Date(event.startTimestamp * 1000).toDateString() === datePickerDate
+  const events: FullEvent[][] = groupEventsByUniqueTournament(
+    data.events.filter((event: FullEvent) => new Date(event.startTimestamp * 1000).toDateString() === datePickerDate)
   );
+
+  const eventsName = events.length === 0 ? 'No events found for this date.' : events[0][0].tournament.category.name;
 
   return (
     <StyledCategoryDetails>
-      <Meta
-        title={events[0].tournament.category.name}
-        description={`${events[0].tournament.category.name} - list of tournaments`}
-      />
-      <h1>{events[0].tournament.category.name}</h1>
+      <Meta title={eventsName} description={`${eventsName} - list of tournaments`} />
+      <h1>{eventsName}</h1>
       <StyledDatePicker>
         <DatePicker />
       </StyledDatePicker>
-      <EventGroups events={events} />
+      <section>
+        {events.map((eventList, i) => {
+          return (
+            <div key={i}>
+              <h3>
+                <UniqueTournamentLink uniqueTournament={eventList[0].tournament.uniqueTournament!} />
+              </h3>
+              <EventGroups events={eventList} />
+            </div>
+          );
+        })}
+      </section>
     </StyledCategoryDetails>
   );
 }
--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -4,43 +4,50 @@ import { SportContext } from '../context/sportContext';
 import { BasicEvent, FullEvent } from '../model/Event';
 import Event from '../modules/Event/Event';
 import EventGroups from '../modules/Event/EventGroups';
+import { groupEventsBySport } from '../util/group';
 
-const StyledFavorites = styled.div`
-  & > h1 {
-    margin-bottom: 2rem;
-    text-align: center;
-  }
+const StyledFavorites = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 2rem;
+  gap: 1rem;
 
-  & > section {
+  & > div {
+    flex-grow: 1;
+    flex-basis: 0;
     background-color: ${({ theme }) => theme.colors.bg1};
     padding: 1rem 2rem;
     border-radius: 10px;
 
-    & > p {
+    & > h3 {
       text-align: center;
     }
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileBreakpoint}) {
-    & > h1 {
-      margin-top: 2rem;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.breakpoint2}) {
+    margin: 1rem;
   }
 `;
 
 export default function Favorites() {
   const sportContext = useContext(SportContext);
+  const eventsBySport = groupEventsBySport(sportContext?.favorites as FullEvent[]);
 
   return (
     <StyledFavorites>
-      <h1>Favorite Events</h1>
-      <section>
-        {sportContext?.favorites.length !== 0 ? (
-          <EventGroups events={sportContext?.favorites as FullEvent[]} />
-        ) : (
-          <p>No favorites found.</p>
-        )}
-      </section>
+      {sportContext?.favorites.length !== 0 ? (
+        eventsBySport.map((events) => {
+          return (
+            <div key={events[0].tournament.category.sport.id}>
+              <h3>{events[0].tournament.category.sport.name}</h3>
+              <EventGroups events={events} />
+            </div>
+          );
+        })
+      ) : (
+        <div>No favorites found.</div>
+      )}
     </StyledFavorites>
   );
 }
